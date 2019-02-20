9ff1a879a779222641a969f6c1ed9bd6afedc877
Johannes Klauss <>
2019-02-20 17:01:30 +0100

    Added docs for react-hotkeys-hook

---
+++ react-hotkeys-hook/static/js/5.885e981a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"./.docz/app/imports.js":function(n,o,t){"use strict";t.r(o),t.d(o,"imports",function(){return s});var s={"docs/intro.mdx":function(){return t.e(1).then(t.bind(null,"./docs/intro.mdx"))},"docs/useHotkeys.mdx":function(){return t.e(2).then(t.bind(null,"./docs/useHotkeys.mdx"))}}}},0,[1,2]]);
+//# sourceMappingURL=5.6befdee158f2f16a3e2f.js.map
